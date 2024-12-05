531427d61f878d943ce39aa43645f717420173cb
Daniel Acosta <danielacosta130602@gmail.com>
2024-12-04 19:21:20 -0500

    fix CMS variables

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,30 +2,32 @@ import "./styles.css";
 
 import React from "react";
 
+import image from "../assets/image.png";
 import GoogleButton from "./google/GoogleButton";
 
-interface LoginComponentProps {
-  backgroundImageUrl: string;
+interface LoginConfig {
+  siteName: string;
+  siteSubtitle: string;
+  loginInstructions: string;
+  contactEmail: string;
 }
 
-const Login: React.FC<LoginComponentProps> = ({ backgroundImageUrl }) => {
+const Login = ({
+  siteName = "Nombre del sitio web",
+  siteSubtitle = "CMS - Gestor de contenidos",
+  loginInstructions = "Accede con tu correo institucional @unal.edu.co, serás redirigido para validar tu identidad.",
+  contactEmail = "XXXXXXXX@unal.edu.co",
+}: LoginConfig) => {
   return (
     <div className="login-container">
       <div className="login-card">
         <div className="login-image-section">
-          <img
-            src={backgroundImageUrl}
-            alt="Login background"
-            className="login-image"
-          />
+          <img src={image} alt="Login background" className="login-image" />
         </div>
         <div className="login-content">
-          <h2 className="login-title">Nombre del sitio web</h2>
-          <p className="login-subtitle">CMS - Gestor de contenidos</p>
-          <p className="login-text">
-            Accede con tu correo institucional @unal.edu.co, serás redirigido
-            para validar tu identidad.
-          </p>
+          <h2 className="login-title">{siteName}</h2>
+          <p className="login-subtitle">{siteSubtitle}</p>
+          <p className="login-text">{loginInstructions}</p>
           <div className="login-google">
             <GoogleButton />
           </div>
@@ -36,7 +38,7 @@ const Login: React.FC<LoginComponentProps> = ({ backgroundImageUrl }) => {
           <p className="access-problems">
             Informar de un problema al siguiente correo
           </p>
-          <p className="access-problems2">XXXXXXXX@unal.edu.co</p>
+          <p className="access-problems2">{contactEmail}</p>
         </div>
       </div>
     </div>
